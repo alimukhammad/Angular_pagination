3609e4c47960be1278214cffc4260689894e8622
sardor <alimukhammad1@gmail.com>
2024-07-08 21:49:52 -0400

    just a button

--- clickButtonEffects/src/app/app.component.ts
+++ clickButtonEffects/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,5 +9,23 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  constructor(private renderer: Renderer2) {}
   title = 'clickButtonEffects';
+  clickCount = 0;
+
+  buttonEffect(){
+      let container = document.querySelector<HTMLElement>('.container');
+      this.clickCount++;
+      /* add border styling to the container */
+      if(container){
+        if(this.clickCount % 2 !== 0){
+          this.renderer.addClass(container, 'borderEffect');
+          this.renderer.removeClass(container, 'noBorder');
+        } else {
+          this.renderer.addClass(container, 'noBorder');
+          this.renderer.removeClass(container, 'borderEffect');
+        }
+      }
+      console.log('Button Clicked');
+  }
 }
