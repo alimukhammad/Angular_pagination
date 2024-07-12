6cdaba4243ec036a471750dd7b532a4cab71e91a
sardor <alimukhammad1@gmail.com>
2024-07-12 00:55:00 -0400

    1

--- clickButtonEffects/src/app/app.component.ts
+++ clickButtonEffects/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component, Renderer2 } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-root', // <app-root></app-root>
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
