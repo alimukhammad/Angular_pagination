a05c87aa3b758f33aac237c673308b0b647a5b2d
sardor <alimukhammad1@gmail.com>
2024-07-12 01:50:56 -0400

    1

--- clickButtonEffects/src/app/app.component.ts
+++ clickButtonEffects/src/app/app.component.ts
@@ -6,9 +6,19 @@ import { Component} from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
- 
+  totalBoxesGroup = 3;
+  boxesPerPage = 3;
+  currentPage = 1;
+
+  get totalPages() {
+    let totalPages = Math.ceil(this.totalBoxesGroup / this.boxesPerPage);
+    //math.ceil is used to round up the number to the nearest integer
+    return totalPages;
+  }
 }
+
+// pagination method starts with the following code snippet
+// 
